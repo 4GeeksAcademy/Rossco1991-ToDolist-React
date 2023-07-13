2a12e418a07259b5682c2da24125645461228a5f
Cody Ross <130109458+Rossco1991@users.noreply.github.com>
2023-07-13 00:48:24 +0000

    Able to get list items to show up and be removed

--- src/js/component/input.jsx
+++ src/js/component/input.jsx
@@ -1,18 +1,39 @@
-import React from "react"
-import { useState } from "react"
+import React from "react";
+import { useState } from "react";
 
-export const Input = (form = []) => {
- 
-    const [input, setInput] = useState('');
-    return(
-        <form onSubmit={e => {
-            e.preventDefault()
-        }}>
-            <ul className="list-group">
-                <input className="form-control" type="text" value={input} onChange={e => setInput(e.target.value)}></input>
-            </ul>
-        </form>
-    )
-}
+export const Input = () => {
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const newTodo = [...todos, input];
+    setTodos(newTodo);
+    setInput("");
+  };
+  
+  const removeTodo = (idx) => {
+    setTodos(todos.toSpliced(idx, 1));
+  };
 
+  return (
+    <form className="index" onSubmit={handleSubmit}>
+      <input
+        className="form-control"
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <ul className="list-group">
+        {todos.map((item, index) => (
+          <li key={index}>
+            {item}
+            <div className="btn btn-primary" onClick={() => removeTodo(index)}>
+              Remove task
+            </div>
+          </li>
+        ))}
+      </ul>
+    </form>
+  );
+};
